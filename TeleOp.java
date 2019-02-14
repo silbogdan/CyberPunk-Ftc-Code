e0f6cfeef910ec5975cae2799150f208af02fc0a
Handrulica <33308597+Handrulica@users.noreply.github.com>
2019-02-15 01:11:46 +0200

    Update TeleOp.java

--- TeleOp.java
+++ TeleOp.java
@@ -50,9 +50,9 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="TeleOp", group="Linear Opmode")
 @Disabled
-public class BasicOpMode_Linear extends LinearOpMode {
+public class TeleOp extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
